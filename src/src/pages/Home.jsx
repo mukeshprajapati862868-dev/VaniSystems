e7e67888bd6358bf468133c53efa87e4c812ad68
mukeshprajapati862868-dev <95376333+mukeshprajapati862868-dev@users.noreply.github.com>
2026-08-08 15:30:38 +0530

    Update Home.jsx
    update

--- src/src/pages/Home.jsx
+++ src/src/pages/Home.jsx
@@ -1,23 +1,50 @@
+// // import React from "react";
+// // import HeroSection from "../components/home/HeroSection.jsx";
+// // import WelcomeSection from "../components/home/WelcomeSection.jsx";
+// // import ServicesHeading from "../components/home/ServicesHeading.jsx";
+// // import ClientsSection from "../components/home/ClientsSection.jsx";
+// // import EventsPhotos from "../components/home/EventsPhotos.jsx";
+
+// // const Home = () => {
+// //   return (
+// //     <>
+// //       <HeroSection />
+// //       <WelcomeSection />
+// //       <ServicesHeading/>
+// //       <ClientsSection/>
+// //       <EventsPhotos/>
+// //     </>
+// //   );
+// // };
+
+// // export default Home;
 // import React from "react";
 // import HeroSection from "../components/home/HeroSection.jsx";
 // import WelcomeSection from "../components/home/WelcomeSection.jsx";
 // import ServicesHeading from "../components/home/ServicesHeading.jsx";
 // import ClientsSection from "../components/home/ClientsSection.jsx";
 // import EventsPhotos from "../components/home/EventsPhotos.jsx";
+// import Products from "./Products";
 
 // const Home = () => {
 //   return (
 //     <>
 //       <HeroSection />
+//       <Products />
 //       <WelcomeSection />
-//       <ServicesHeading/>
-//       <ClientsSection/>
-//       <EventsPhotos/>
+//       <ServicesHeading />
+//       <ClientsSection />
+//       <EventsPhotos />
+
+//       {/* Products Section */}
+//       {/* <Products /> */}
 //     </>
 //   );
 // };
 
 // export default Home;
+
+
 import React from "react";
 import HeroSection from "../components/home/HeroSection.jsx";
 import WelcomeSection from "../components/home/WelcomeSection.jsx";
@@ -34,6 +61,7 @@ const Home = () => {
       <WelcomeSection />
       <ServicesHeading />
       <ClientsSection />
+      <EventsPhotos/>
       <EventsPhotos />
 
       {/* Products Section */}
@@ -43,3 +71,6 @@ const Home = () => {
 };
 
 export default Home;
+
+
+
